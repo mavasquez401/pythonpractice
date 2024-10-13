e03ee98e929903bca3d87a12abbc7e38701b581b
Manuel Vasquez <163361779+mavasquez401@users.noreply.github.com>
2024-10-12 20:08:20 -0400

    simple math problems

---
+++ mathing.py
@@ -0,0 +1,24 @@
+x = 5
+y = 15
+
+def add(x, y):
+    return x + y
+print(add(x, y))
+
+def sub(x, y):
+    return x - y
+print(sub(x, y))
+
+def mult(x, y):
+    return x * y
+print(mult(x, y))
+
+def div(x, y):
+    return x / y
+print(div(x, y))
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
